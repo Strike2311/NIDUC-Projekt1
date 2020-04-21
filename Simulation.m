2dc1c66a588715067ef008f29d5cf6d426e154ef
Strike2311 <49807482+Strike2311@users.noreply.github.com>
2020-04-21 17:25:47 +0200

    Added Comments To Code

--- Simulation.m
+++ Simulation.m
@@ -10,19 +10,20 @@ hammingCodeTestBER = zeros(1,150);
 
 
 for i=1:tests
+    %kodowanie dwoma sposobami tego samego ciagu liczb
     tripleCode = TripleEncoder(inputArray);
     hammingCode = HammingEncoder(inputArray);
-    
+    % 'przepuszczanie' wygenerowanych ciagow przez kanal transmisyjny
     tripleCode = GilbertChannel(tripleCode);
     hammingCode = GilbertChannel(hammingCode);
-    
+    %odkodowanie obu ciagow
     outputTripleArray = TripleDecoder(tripleCode);
     outputHammingArray = HammingDecoder(hammingCode);
-    
+    %obliczanie BER dla obu ciagow 
     tripleCodeTestBER(i) = BER(inputArray,outputTripleArray);
     hammingCodeTestBER(i) = BER(inputArray,outputHammingArray);
 end
-
+%wyniki eksperymentu w postaci srednich wszystkich wynikow
 x = "Srednie BER dla kodu potrajajacego: "
 mean(tripleCodeTestBER)
 x = "Srednie BER dla kodu hamminga: "
--- TripleEncoder.m
+++ TripleEncoder.m
@@ -1,8 +1,8 @@
 function [Y] = TripleEncoder(X)
-    counter = 1;
-    Y = zeros(1, 3*length(X));
+    counter = 1;%zmienna sluzaca do poruszania sie po tabeli Y
+    Y = zeros(1, 3*length(X));%do tej tabeli wprowadzamy zakodowywana liczbe
     for i=1:length(X)
-        
+        %potrajanie bitow i wypelnianie nimi tabeli Y
         Y(counter:counter+2) = X(i);
         counter = counter + 3;
         
